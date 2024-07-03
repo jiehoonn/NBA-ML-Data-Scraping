b92c0c79d6cac8b894839e53a5f1fccdb18edf7d
Jiehoon Lee <jiehoonn@bu.edu>
2024-07-03 18:12:54 -0400

    extracted data from html files and combined to one csv

--- web_scraping.py
+++ web_scraping.py
@@ -1,8 +1,10 @@
+# First Run
 import requests
 import os
 import time
 from bs4 import BeautifulSoup
 import pandas as pd
+from io import StringIO
 
 # Define the years range
 years = list(range(1991, 2025))
@@ -29,3 +31,33 @@ for year in years:
     
     time.sleep(20)  # Delay to prevent rate limiting
 
+# Second Run: Process the downloaded HTML files
+dfs = []
+for year in years:
+    try:
+        with open(f"mvp/{year}.html", encoding='utf-8') as f:
+            page = f.read()
+        soup = BeautifulSoup(page, "html.parser")
+        
+        # Check if 'over_header' exists and remove it if it does
+        over_header = soup.find('tr', class_="over_header")
+        if over_header:
+            over_header.decompose()
+        
+        # Find the MVP table
+        mvp_table = soup.find('table', id="mvp")
+        if not mvp_table:
+            print(f"No MVP table found for the year {year}")
+            continue
+        
+        # Convert the MVP row to a DataFrame
+        mvp = pd.read_html(StringIO(str(mvp_table)))[0]
+        dfs.append(mvp)
+    except FileNotFoundError:
+        print(f"File not found for the year {year}")
+    except Exception as e:
+        print(f"Error processing data for the year {year}: {e}")
+
+mvps = pd.concat(dfs)
+mvps.tail()
+mvps.to_csv("mvp.csv")
